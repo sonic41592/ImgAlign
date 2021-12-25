af466b11be4d97ab32b1508b824678665754f2ed
sonic41592 <sonic41092@gmail.com>
2021-12-24 21:23:25 -0500

    Update ImgAlign.py

--- ImgAlign.py
+++ ImgAlign.py
@@ -18,6 +18,8 @@ parser.add_argument("-r", "--rotate", action='store_true', default=False, help="
 parser.add_argument("-g", "--hr", default='', help="HR File or folder directory.  No need to use if they are in HR folder in current working directory.")
 parser.add_argument("-l", "--lr", default='', help="LR File or folder directory.  No need to use if they are in LR folder in current working directory.")
 parser.add_argument("-o", "--overlay", action='store_true', default=False, help="Disabled by default.  After saving aligned images, this option will create a separate 50:50 merge of the aligned images in the Overlay folder. Useful for quickly checking through image sets for poorly aligned outputs")
+parser.add_argument("-f", "--full", action='store_true', default=False, help="Disabled by default.  If enabled, this allows full homography mapping of the image, correcting rotations, translations, and warping.")
+
 
 args = vars(parser.parse_args())
 
@@ -29,6 +31,10 @@ rotate = args["rotate"]
 HRfolder = args["hr"]
 LRfolder = args["lr"]
 Overlay = args["overlay"]
+Homography = args["full"]
+
+if Homography == True:
+  rotate = False
 
 MAX_FEATURES = 500
 GOOD_MATCH_PERCENT = 0.15
@@ -36,11 +42,8 @@ GOOD_MATCH_PERCENT = 0.15
 
 def AutoCrop(image):
     """Crops any edges below or equal to threshold
-
     Crops blank image to 1x1.
-
     Returns cropped image.
-
     """
     threshold = lumthresh
     if len(image.shape) == 3:
@@ -58,38 +61,198 @@ def AutoCrop(image):
 
     return image
 
-def Find_Affine(im1, im2):
+def Find_Homography(im1, im2):
+  
+  im1y, im1x, _ = im1.shape
+  im2y, im2x, _ = im2.shape
+  im0 = im1[:]
+  im00 = im2[:]
+  
+  # Double lower resolution dummy image if the scale is too big for cv2 to match reliably
+  if scale == 4 and mode == 0:
+    im00 = cv2.resize(im2,(round(2*im2x),round(2*im2y)),interpolation=cv2.INTER_CUBIC)
+  if scale == 4 and mode == 1:
+    im0 = cv2.resize(im1,(round(2*im1x),round(2*im1y)),interpolation=cv2.INTER_CUBIC)
   
   # Convert images to grayscale
-  im1Gray = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
-  im2Gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
-
-  # Detect ORB features and compute descriptors.
-  orb = cv2.ORB_create(MAX_FEATURES)
+  im1Gray = cv2.cvtColor(im0, cv2.COLOR_BGR2GRAY)
+  im2Gray = cv2.cvtColor(im00, cv2.COLOR_BGR2GRAY)
+  
+  # Detect SIFT features and compute descriptors.
+  orb = cv2.SIFT_create(MAX_FEATURES)
   keypoints1, descriptors1 = orb.detectAndCompute(im1Gray, None)
   keypoints2, descriptors2 = orb.detectAndCompute(im2Gray, None)
-
-  # Match features.
-  matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
-  matches = matcher.match(descriptors1, descriptors2, None)
-
-  # Sort matches by score
-  matches.sort(key=lambda x: x.distance, reverse=False)
-
-  # Remove not so good matches
-  numGoodMatches = int(len(matches) * GOOD_MATCH_PERCENT)
-  matches = matches[:numGoodMatches]
   
-  # Extract location of good matches
-  points1 = np.zeros((len(matches), 2), dtype=np.float32)
-  points2 = np.zeros((len(matches), 2), dtype=np.float32)
+  # SIFT feature matching
+  bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=False)
+  matches = bf.knnMatch(descriptors1,descriptors2,k=2)
+  
+  good = []
+  for m,n in matches:
+    if m.distance < 0.7*n.distance:
+      good.append(m)
+  
+  if len(good) > 5:
+    points1 = np.float32([ keypoints1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    points2 = np.float32([ keypoints2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+  
+  # Find homography
+  h, mask = cv2.findHomography(points1, points2, cv2.RANSAC)
+  # Fix upscaled homography
+  if scale == 4 and mode == 0:
+    h = np.array([[0.5,0,0],[0,0.5,0],[0,0,1]]) @ h
+  if scale == 4 and mode == 1:
+    h = np.array([[2,0,0],[0,2,0],[0,0,1]]) @ h
+  
+  # print(h)
+  
+  # Get original image dimensions 
+  height, width, _ = im1.shape
 
-  for i, match in enumerate(matches):
-    points1[i, :] = keypoints1[match.queryIdx].pt
-    points2[i, :] = keypoints2[match.trainIdx].pt
+  # Find corners of transformed image
+  a = np.array([[0,0],[0,height],[width,height],[width,0]],dtype='float32')
+  a = np.array([a])
+  A = cv2.perspectiveTransform(a,h)
+
+  # Get distances between points to scale homograpy matrix so that the most reduced edge matches resolution of original image
+  sab = (np.sqrt((A[0,0,0]-A[0,1,0])**2+(A[0,0,1]-A[0,1,1])**2))/height
+  sbc = (np.sqrt((A[0,1,0]-A[0,2,0])**2+(A[0,1,1]-A[0,2,1])**2))/width
+  scd = (np.sqrt((A[0,2,0]-A[0,3,0])**2+(A[0,2,1]-A[0,3,1])**2))/height
+  sda = (np.sqrt((A[0,3,0]-A[0,0,0])**2+(A[0,3,1]-A[0,0,1])**2))/width
+  scaleR = 1/min(sab,sbc,scd,sda)
+  smat = np.array([[scaleR,0,0],[0,scaleR,0],[0,0,1]])
+  h = smat @ h
+
+  # Find new corners and translation components of proper scale transformation 
+  A = cv2.perspectiveTransform(a,h)
+  [xmin, ymin] = np.array([min(A[0,:,0])-1, min(A[0,:,1])-1],dtype='int32')
+  [xmax, ymax] = np.array([max(A[0,:,0])+1, max(A[0,:,1])+1],dtype='int32')
+
+  # Translate homography to x and y axis
+  T = np.array([[1,0,-xmin],[0,1,-ymin],[0,0,1]])
+  h = T @ h
+
+  # Generate, warp, and find new dimensions of a white image with same dimensions as original image
+  white = np.zeros([height,width,3],dtype=np.uint8)
+  white.fill(255)
+  warpwhite = cv2.warpPerspective(white,h,(xmax-xmin,ymax-ymin),flags=0)
+  wh, ww, _ = warpwhite.shape
+  a1 = [0,0] 
+  b1 = [0,wh]
+  c1 = [ww,wh]
+  d1 = [ww,0]
+  
+  # Shrink corners until contained within white boundaries
+  while max(warpwhite[a1[1],a1[0]]) == 0 or max(warpwhite[b1[1],b1[0]]) == 0 or max(warpwhite[c1[1],c1[0]]) == 0 or max(warpwhite[d1[1],d1[0]]) == 0:
+    a1[0] = a1[0] + 1
+    a1[1] = a1[1] + 1
+    b1[0] = b1[0] + 1
+    b1[1] = b1[1] - 1
+    c1[0] = c1[0] - 1
+    c1[1] = c1[1] - 1
+    d1[0] = d1[0] - 1
+    d1[1] = d1[1] + 1
+
+  # Boundary testing variables
+  sA = a1[:]
+  sB = b1[:]
+  sC = c1[:]
+  sD = d1[:]
+  tA = a1[:]
+  tB = b1[:]
+  tC = c1[:]
+  tD = d1[:]
+
+  # Individually increase edge boundaries to maximize area
+  while sA[1] > 0 and (max(warpwhite[sA[1]-1,sA[0]]) > 0 and max(warpwhite[sD[1]-1,sD[0]]) > 0):
+    sA[1] = sA[1] - 1
+    sD[1] = sD[1] - 1
+  while sB[1] < wh and (max(warpwhite[sB[1]+1,sB[0]]) > 0 and max(warpwhite[sC[1]+1,sC[0]]) > 0):
+    sB[1] = sB[1] + 1
+    sC[1] = sC[1] + 1
+  while sA[0] > 0 and (max(warpwhite[sA[1],sA[0]-1]) > 0 and max(warpwhite[sB[1],sB[0]-1]) > 0):
+    sA[0] = sA[0] - 1
+    sB[0] = sB[0] - 1
+  while sD[0] < ww and (max(warpwhite[sD[1],sD[0]+1]) > 0 and max(warpwhite[sC[1],sC[0]+1]) > 0):
+    sD[0] = sD[0] + 1
+    sC[0] = sC[0] + 1
+
+  while tA[0] > 0 and (max(warpwhite[tA[1],tA[0]-1]) > 0 and max(warpwhite[tB[1],tB[0]-1]) > 0):
+    tA[0] = tA[0] - 1
+    tB[0] = tB[0] - 1
+  while tD[0] < ww and (max(warpwhite[tD[1],tD[0]+1]) > 0 and max(warpwhite[tC[1],tC[0]+1]) > 0):
+    tD[0] = tD[0] + 1
+    tC[0] = tC[0] + 1
+  while tA[1] > 0 and (max(warpwhite[tA[1]-1,tA[0]]) > 0 and max(warpwhite[tD[1]-1,tD[0]]) > 0):
+    tA[1] = tA[1] - 1
+    tD[1] = tD[1] - 1
+  while tB[1] < wh and (max(warpwhite[tB[1]+1,tB[0]]) > 0 and max(warpwhite[tC[1]+1,tC[0]]) > 0):
+    tB[1] = tB[1] + 1
+    tC[1] = tC[1] + 1
+
+  # Choose biggest bound image
+  ssize = (sC[0]-sA[0])*(sC[1]-sA[1])
+  tsize = (tC[0]-tA[0])*(tC[1]-tA[1])
+
+  if tsize >= ssize:
+    a1 = tA[:]
+    b1 = tB[:]
+    c1 = tC[:]
+    d1 = tD[:]
+  else:
+    a1 = sA[:]
+    b1 = sB[:]
+    c1 = sC[:]
+    d1 = sD[:]
+
+  # Warp original image
+  output = cv2.warpPerspective(im1,h,(ww,wh),flags=2)
+  Im1 = output[a1[1]:c1[1],a1[0]:c1[0]]
+  
+  return Im1
+
+def Find_Affine(im1, im2):
+  
+  im1y, im1x, _ = im1.shape
+  im2y, im2x, _ = im2.shape
+  im0 = im1[:]
+  im00 = im2[:]
+  
+  # Double lower resolution dummy image if the scale is too big for cv2 to match reliably
+  if scale == 4 and mode == 0:
+    im00 = cv2.resize(im2,(round(2*im2x),round(2*im2y)),interpolation=cv2.INTER_CUBIC)
+  if scale == 4 and mode == 1:
+    im0 = cv2.resize(im1,(round(2*im1x),round(2*im1y)),interpolation=cv2.INTER_CUBIC)
+  
+  # Convert images to grayscale
+  im1Gray = cv2.cvtColor(im0, cv2.COLOR_BGR2GRAY)
+  im2Gray = cv2.cvtColor(im00, cv2.COLOR_BGR2GRAY)
+  
+  # Detect SIFT features and compute descriptors.
+  orb = cv2.SIFT_create(MAX_FEATURES)
+  keypoints1, descriptors1 = orb.detectAndCompute(im1Gray, None)
+  keypoints2, descriptors2 = orb.detectAndCompute(im2Gray, None)
+  
+  # SIFT feature matching
+  bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=False)
+  matches = bf.knnMatch(descriptors1,descriptors2,k=2)
+  
+  good = []
+  for m,n in matches:
+    if m.distance < 0.7*n.distance:
+      good.append(m)
+  
+  if len(good) > 5:
+    points1 = np.float32([ keypoints1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    points2 = np.float32([ keypoints2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
 
   # Find homography
-  h, mask = cv2.estimateAffine2D(points1, points2)
+  h, mask = cv2.estimateAffine2D(points1, points2, cv2.RANSAC)
+  # Fix upscaled homography
+  if scale == 4 and mode == 0:
+    h = np.array([[0.5,0],[0,0.5]]) @ h
+  if scale == 4 and mode == 1:
+    h = np.array([[2,0],[0,2]]) @ h
   
   return h
 
@@ -148,36 +311,46 @@ def Fix_Rotation(Affine, img):
 
 def maint_lr_res(im1, im2):
   
+  im1y, im1x, _ = im1.shape
+  im2y, im2x, _ = im2.shape
+  im0 = im1[:]
+  im00 = im2[:]
+  
+  # Double lower resolution dummy image if the scale is too big for cv2 to match reliably
+  if scale == 4 and mode == 0:
+    im00 = cv2.resize(im2,(round(2*im2x),round(2*im2y)),interpolation=cv2.INTER_CUBIC)
+  if scale == 4 and mode == 1:
+    im0 = cv2.resize(im1,(round(2*im1x),round(2*im1y)),interpolation=cv2.INTER_CUBIC)
+  
   # Convert images to grayscale
-  im1Gray = cv2.cvtColor(im1, cv2.COLOR_BGR2GRAY)
-  im2Gray = cv2.cvtColor(im2, cv2.COLOR_BGR2GRAY)
-
-  # Detect ORB features and compute descriptors.
-  orb = cv2.ORB_create(MAX_FEATURES)
+  im1Gray = cv2.cvtColor(im0, cv2.COLOR_BGR2GRAY)
+  im2Gray = cv2.cvtColor(im00, cv2.COLOR_BGR2GRAY)
+  
+  # Detect SIFT features and compute descriptors.
+  orb = cv2.SIFT_create(MAX_FEATURES)
   keypoints1, descriptors1 = orb.detectAndCompute(im1Gray, None)
   keypoints2, descriptors2 = orb.detectAndCompute(im2Gray, None)
-
-  # Match features.
-  matcher = cv2.DescriptorMatcher_create(cv2.DESCRIPTOR_MATCHER_BRUTEFORCE_HAMMING)
-  matches = matcher.match(descriptors1, descriptors2, None)
-
-  # Sort matches by score
-  matches.sort(key=lambda x: x.distance, reverse=False)
-
-  # Remove not so good matches
-  numGoodMatches = int(len(matches) * GOOD_MATCH_PERCENT)
-  matches = matches[:numGoodMatches]
   
-  # Extract location of good matches
-  points1 = np.zeros((len(matches), 2), dtype=np.float32)
-  points2 = np.zeros((len(matches), 2), dtype=np.float32)
-
-  for i, match in enumerate(matches):
-    points1[i, :] = keypoints1[match.queryIdx].pt
-    points2[i, :] = keypoints2[match.trainIdx].pt
-
+  # SIFT feature matching
+  bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=False)
+  matches = bf.knnMatch(descriptors1,descriptors2,k=2)
+  
+  good = []
+  for m,n in matches:
+    if m.distance < 0.7*n.distance:
+      good.append(m)
+  
+  if len(good) > 5:
+    points1 = np.float32([ keypoints1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+    points2 = np.float32([ keypoints2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+  
   # Find homography
-  h, mask = cv2.estimateAffine2D(points1, points2)
+  h, mask = cv2.estimateAffine2D(points1, points2, cv2.RANSAC)
+  # Fix upscaled homography
+  if scale == 4 and mode == 0:
+    h = np.array([[0.5,0],[0,0.5]]) @ h
+  if scale == 4 and mode == 1:
+    h = np.array([[2,0],[0,2]]) @ h  
   
   # Remove rotation 
   sx = math.sqrt(h[0,0]**2+h[1,0]**2)
@@ -256,15 +429,19 @@ def Do_Work(hrimg, lrimg):
   highres = cv2.imread(hrimg, cv2.IMREAD_COLOR)
   lowres = cv2.imread(lrimg, cv2.IMREAD_COLOR)
   
-  if autocrop == True:
-    highres = AutoCrop(highres)
-    lowres = AutoCrop(lowres)
   
   if mode == 0:
+  
+    if autocrop == True:
+      highres = AutoCrop(highres)
+      lowres = AutoCrop(lowres)
     
     if rotate == True:
       Aff = Find_Affine(highres,lowres)
       highres = Fix_Rotation(Aff, highres)
+    
+    if Homography == True:
+      highres = Find_Homography(highres,lowres)
     
     crop_lr, scalehr = maint_lr_res(highres, lowres)
     cv2.imwrite('Output\\LR\\{:s}.png'.format(base), crop_lr)
@@ -272,9 +449,16 @@ def Do_Work(hrimg, lrimg):
   
   if mode == 1:
   
+    if autocrop == True:
+      highres = AutoCrop(highres)
+      lowres = AutoCrop(lowres)
+  
     if rotate == True:
       Aff = Find_Affine(lowres,highres)
       lowres = Fix_Rotation(Aff, lowres)
+    
+    if Homography == True:
+      lowres = Find_Homography(lowres,highres)
     
     crop_lr, scalehr = maint_lr_res(lowres,highres)
     cv2.imwrite('Output\\HR\\{:s}.png'.format(base), crop_lr)
@@ -313,7 +497,6 @@ if os.path.isfile(HRfolder) == True:
   lrim = LRfolder
   Do_Work(hrim, lrim)
 
-
 else:
     # Batch processing for all images in HR folder
   if len(HRfolder) == 0:
@@ -326,4 +509,12 @@ else:
     hrim = path
     lrim = LRfolder+'/'+base+extention
     print('{:s}'.format(base)+extention)
-    Do_Work(hrim, lrim)
+    try:
+      Do_Work(hrim, lrim)
+    except KeyboardInterrupt:
+      break
+    except:
+      with open('Output\Failed.txt', 'a+') as f:
+        f.write('{:s}'.format(base)+extention+'\n')
+        f.close()
+      print('Match failed for ','{:s}'.format(base)+extention)
